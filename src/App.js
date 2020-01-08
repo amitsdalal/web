6f734cd6affdd29c91b9c49d5ab5d2eed4544443
Amit Dalal <sameerashle@gmail.com>
2020-01-08 18:12:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Amit Dalal's Web!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/amitsdalal"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Work!
         </a>
       </header>
     </div>
