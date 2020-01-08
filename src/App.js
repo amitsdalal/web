fb02eb17d6911f5206cf1b4662cf4e3b2123cbc3
Amit Dalal <sameerashle@gmail.com>
2020-01-08 18:23:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Amit Dalal's Web!
+          Welcome to Amit Dalal Web.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          My Work!
+          My Work.
         </a>
       </header>
     </div>
