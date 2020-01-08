69c09a429c4b8bb2233b3307b08f81fbb7848a03
Amit Singh Dalal <amit.singh@magehost.com>
2020-01-08 17:28:09 +0530

    React App start

---
+++ precache-manifest.3352d5b1af83a28923d7ec0815f3935b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3031da135eb710d1c410129bd534532a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36ee5493cff31a3ccb18",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d222c4b0a26aeb88907e",
+    "url": "/static/js/2.0f20b8d5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0f20b8d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36ee5493cff31a3ccb18",
+    "url": "/static/js/main.fa1e5137.chunk.js"
+  },
+  {
+    "revision": "8ffc8803dbe9ff207126",
+    "url": "/static/js/runtime-main.0c5ab20e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
